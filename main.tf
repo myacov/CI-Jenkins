41cff182d5ea6a5a5a22105ac962b3406a63e9c3
Yacov <m.yacov@gmail.com>
2023-11-01 11:00:32 +0200

    fix name error

--- main.tf
+++ main.tf
@@ -9,8 +9,8 @@ resource "aws_vpc" "myapp-vpc" {
     }
 }
 
-resource "aws_subnet" "dev-subnet-1" {
-    vpc_id     = aws_vpc.dev-vpc.id
+resource "aws_subnet" "myapp-subnet-1" {
+    vpc_id     = aws_vpc.myapp-vpc.id
     cidr_block = var.subnet_cidr_block
     availability_zone = var.avail_zone
     tags = {
