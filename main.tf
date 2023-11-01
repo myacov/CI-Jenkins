4d77c5a7448e7bf6c6e286a68d3de8f786ab9be7
Yacov <m.yacov@gmail.com>
2023-11-01 11:23:32 +0200

    add rt and igw

--- main.tf
+++ main.tf
@@ -17,3 +17,21 @@ resource "aws_subnet" "myapp-subnet-1" {
     Name = "${var.env_prefix}-subnet-1"
   }
 }
+
+resource "aws_route_table" "myapp-route-table" {
+    vpc_id =  aws_vpc.myapp-vpc.id
+    route {
+        cidr_block = "0.0.0.0/0"
+        gateway_id = myapp-igw.id
+    }
+    tags = {
+        Name = "${var.env_prefix}-rtb"
+  }
+}
+
+resource "aws_internet_gateway" "myapp-igw" {
+    vpc_id = aws_vpc.myapp-vpc.id
+    tags = {
+        Name = "${var.env_prefix}-igw"
+  }
+}
