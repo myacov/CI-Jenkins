68573ec04140b520e86867db3d73c301476c142f
Yacov <m.yacov@gmail.com>
2023-11-01 09:10:51 +0200

    terraform providers and vars

---
+++ vars.tf
@@ -0,0 +1,32 @@
+variable "AWS_REGION" {
+  default = "us-east-1"
+}
+
+variable "AMIS" {
+  type = map(any)
+  default = {
+    us-east-1 = "ami-0fc5d935ebf8bc3bc"
+    us-east-2 = "ami-0e83be366243f524a"
+  }
+}
+
+variable "PRIV_KEY_PATH" {
+  default = "ci-vprofile-key"
+}
+
+variable "PUB_KEY_PATH" {
+  default = "ci-vprofile-key.pub"
+}
+
+variable "USERNAME" {
+  default = "ubuntu"
+}
+
+variable "MYIP" {
+  default = "31.187.78.213/32"
+
+}
+
+variable "instance_count" {
+  default = "1"
+}
